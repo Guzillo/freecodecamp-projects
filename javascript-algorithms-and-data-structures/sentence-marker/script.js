71f9956bfee84885f95dcf62df443a084bf932d6
guzillo <guzillo2077@gmail.com>
2025-04-02 21:04:24 +0200

    add: sentence marker project
    this project focuses on assigning, declaring, reassigning variables and string concatenation

---
+++ javascript-algorithms-and-data-structures/sentence-marker/script.js
@@ -0,0 +1,20 @@
+let adjective = "red";
+let noun = "dragon";
+let verb = "cold";
+let place = "Norway"; 
+let adjective2 = "huge";
+let noun2 = "fruit"; 
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." +
+" The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "blue";
+noun = "cat";
+verb = "dark";
+place = "England";
+adjective2 = "small";
+noun2 = "grass";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." +
+" The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
