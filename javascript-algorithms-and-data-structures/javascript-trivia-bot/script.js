fe351f36731998e993cb67b2c988c22918dc8946
guzillo <guzillo2077@gmail.com>
2025-04-02 20:43:40 +0200

    add: javascript trivia bot project
    add project that mainly focus is on js variables and strings practice

---
+++ javascript-algorithms-and-data-structures/javascript-trivia-bot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Neura";
+let botLocation = "cloud";
+let favoriteLanguage = "Go";
+
+console.log(
+  "My name is " + botName + " and I live on " + botLocation + "."
+);
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I just love " + favoriteLanguage + " programming language!" + " It's so simple, fast and has built in garbage collector.";
+console.log(codingFact);
+
+codingFact = "Thanks to its simple syntax and efficient compiler, " + favoriteLanguage + " turns code into machine code much quicker than languages like Java or C++. "
+console.log(codingFact);
+// 9 
+codingFact = favoriteLanguage + " was inspired by C but made simpler and more modern!"
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
